2e4adbf3ebdb20ec29c697ff3b3d7f02f7b6fa86
Jason Sayers <JasonJSayers@gmail.com>
2012-09-29 18:12:20 -0500

    Application currently selects the correct messages to send an email event on. Next step is to add logic to send e-mails out.

--- EventNotification/ErrorMessage.cs
+++ EventNotification/ErrorMessage.cs
@@ -5,38 +5,52 @@ using System.Text;
 
 namespace EventNotification
 {
+    // Class for parsing out error messages
     class ErrorMessage
     {
         public ErrorMessage(string error)
         {
-            _error = error;
+            // Split the error into chunks based on pipes in message
             string[] chunks = error.Split('|');
 
+            // Check that we are able to parse the date and that there are at least 4 sections
             if (chunks.Length > 3 && DateTime.TryParse(chunks[0], out _date))
             {
                 _key1 = chunks[1];
                 _key2 = chunks[2];
-                _message = chunks[3];
+                _description = chunks[3];
+
+                _fullKey = _key1 + Key2;
             }
             else _parsingError = true;
         }
 
+        // holds date and time of error
         private DateTime _date;
         public DateTime Date { get { return _date; } }
 
+        private string _fullKey;
+        public string FullKey { get { return _fullKey; } }
+
+        // Holds the first key
         private string _key1;
         public string Key1 { get { return _key1; } }
 
+        // Holds the second key
         private string _key2;
         public string Key2 { get { return _key2; } }
 
-        private string _message;
-        public string Message { get { return _message; } }
+        // Hold the error message
+        private string _description;
+        public string Description { get { return _description; } }
 
-        private string _error;
-        public string Error { get { return _error; } }
-
+        // Indicates if the error was formatted incorrectly
         public bool _parsingError = false;
         public bool ParsingError { get { return _parsingError; } }
+
+        public override string ToString()
+        {
+            return _date.ToString("MM/dd/yyyy HH:mm:ss") + "|" + _key1 + "|" + _key2 + "|" + _description;
+        }
     }
 }
--- EventNotification/Program.cs
+++ EventNotification/Program.cs
@@ -9,6 +9,8 @@ namespace EventNotification
     class Program
     {
         private static int errorCount = 0;
+        private static int warningCount = 0;
+        private static Dictionary<string, List<DateTime>> messages = new Dictionary<string,List<DateTime>>();
 
         static void Main(string[] args)
         {
@@ -38,19 +40,6 @@ namespace EventNotification
 #endif
         }
 
-        // Writes a line to the console
-        private static void Info(string infoMessage)
-        {
-            Console.WriteLine(infoMessage);
-        }
-
-        // Writes a line to the console and increments the number of errors
-        private static void Error(string errorMessage)
-        {
-            ++errorCount;
-            Console.WriteLine(errorMessage);
-        }
-
         // Returns true if the number of errors is more than 0
         private static bool HasErrors { get { return errorCount > 0; } }
 
@@ -68,28 +57,76 @@ namespace EventNotification
 
                     while (!input.EndOfStream)
                     {
-                        string line = input.ReadLine();
+                        // Get next line
+                        string message = input.ReadLine();
 
-                        try
-                        {
+                        // Parse error message
+                        ErrorMessage errMsg = new ErrorMessage(message);
 
-                            ErrorMessage myError = new ErrorMessage(line);
-                        }
-                        catch (Exception ex)
+                        // Check that message is ok
+                        if (!errMsg.ParsingError)
                         {
-                            Error("Error encountered while line" + ex.ToString());
+                            // Add error to list of similar messages and check for threshold
+                            CheckForEmail(errMsg);
                         }
+                        else Warn("Unable to parse record - '" + message + "'");
                     }
-
-
                 }
                 else Error("Log file '" + CommandLineSettings.LogFile + "' does not exist. Program terminating.");
-
             }
             catch (Exception ex)
             {
-                Error("Error encountered while parsing file: \r\n" + ex.ToString()); 
+                Error("Error encountered while parsing file: \r\n" + ex.ToString());
             }
+        }
+
+        private static void CheckForEmail(ErrorMessage errMsg)
+        {
+            List<DateTime> currentErrorList;                // Holds current error list
+            DateTime cutOff = errMsg.Date.AddMinutes(-5);   // Holds current cut off time
+
+            // Add a list for error type if it currently does not exist
+            if (!messages.ContainsKey(errMsg.FullKey))
+                messages.Add(errMsg.FullKey, new List<DateTime>());
+
+            // Selected the list that matches current error type
+            currentErrorList = messages[errMsg.FullKey];
+
+            // Remove entries that are 5 min older then current error
+            for (int i = currentErrorList.Count - 1; i > -1; --i)
+                if (currentErrorList[i] < cutOff)
+                    currentErrorList.RemoveAt(i);
+
+            // Add current error to list.
+            currentErrorList.Add(errMsg.Date);
+
+            // Check to see if list meets threshold required
+            //  if it is send email and reset list for current error type
+            if (currentErrorList.Count >= CommandLineSettings.Threshold)
+            {
+                Console.WriteLine(errMsg.ToString());
+                currentErrorList.Clear();
+            }
+        }
+
+        // Writes a line to the console
+        private static void Info(string infoMessage)
+        {
+            Console.WriteLine("[Info] " + infoMessage);
+        }
+
+        // Writes a line to the console and increments the number of warnings
+        private static void Warn(string warningMessage)
+        {
+            ++warningCount;
+            Console.WriteLine("[Warning] " + warningMessage);
+        }
+
+        // Writes a line to the console and increments the number of errors
+        private static void Error(string errorMessage)
+        {
+            ++errorCount;
+            Console.WriteLine("[Error] " + errorMessage);
         }
     }
 }
